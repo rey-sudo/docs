dc8f43a1512bfb686cf2bf79a06c3a2ec747e562
Juan Rey <sudo.delete.evil@gmail.com>
2025-07-21 12:45:44 -0500

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Pairfy - Marketplace",
+  base: '/docs/',
   description: "Buy and sell products on the Cardano blockchain",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
